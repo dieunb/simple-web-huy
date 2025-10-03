5725b7f8acc7403b70e62245b33195cad4693463
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-03 22:00:49 +0700

    router: implement request method override handling

--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -8,8 +8,26 @@ module Frack
       end
 
       def call(env)
-
+        path = env['PATH_INFO']
+        http_method = env['REQUEST_METHOD']
+      
+      if http_method == 'POST'
+        request = Rack::Request.new(env)
+        override_method = request.params['_method']
+        if override_method && ['DELETE', 'PATCH', 'PUT'].include?(override_method.upcase)
+          http_method = override_method.upcase
+        end
       end
+      
+      env['REQUEST_METHOD'] = http_method
+      
+      if mapping = routes[path+http_method]
+        env.merge!(controller_action(mapping))
+        app.call(env)
+      else
+        Rack::Response.new('Not found', 404).finish
+      end
+    end
 
       def controller_action(mapping)
       Hash[%w(controller action).zip mapping.split('#')]
