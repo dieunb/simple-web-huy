94cd30869dad33aece3e1648fc8737d1210bcdfb
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-03 21:44:22 +0700

    router: implement HTTP method routes

--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -8,9 +8,36 @@ module Frack
       end
 
       def call(env)
-        
+
       end
 
       def controller_action(mapping)
       Hash[%w(controller action).zip mapping.split('#')]
       end
+
+      def put(route) 
+        self.routes
+            .merge!(route.keys.first + 'PUT' => route.values.first)
+      end
+
+      def get(route)
+        self.routes
+            .merge!(route.keys.first + 'GET' => route.values.first)
+      end
+
+      def post(route)
+        self.routes
+            .merge!(route.keys.first + 'POST' => route.values.first)
+      end
+
+      def delete(route)
+        self.routes
+            .merge!(route.keys.first + 'DELETE' => route.values.first)  
+      end
+
+      def get(route)
+        self.routes
+            .merge!(route.keys.first + 'GET' => route.values.first)
+      end
+  end
+end 
