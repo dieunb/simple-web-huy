6c8ce82ea4f16db0b6349dd7bc2f57fe5be9f5e5
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-05 13:56:49 +0700

    fix ActiveRecord require

--- lib/frack.rb
+++ lib/frack.rb
@@ -3,8 +3,9 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 require 'rack'
 require 'tilt'
 require 'erb'
-require 'activerecord'
+require 'active_record'
 require 'pg'
+require 'otr-activerecord'
 
 OTR::ActiveRecord.configure_from_file!('config/database.yml')
 OTR::ActiveRecord.establish_connection!
