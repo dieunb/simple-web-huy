c4f9728a2a99487533787d556d7a532b3e29ff40
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-11 01:04:12 +0700

    Apply RuboCop auto-corrections

--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Frack
   class Router
     attr_reader :app, :routes
@@ -7,22 +9,20 @@ module Frack
       @routes = {}
       instance_eval(&block) if block_given?
     end
-    
+
     def call(env)
       path = env['PATH_INFO']
       http_method = env['REQUEST_METHOD']
-      
+
       if http_method == 'POST'
         request = Rack::Request.new(env)
         override_method = request.params['_method']
-        if override_method && ['DELETE', 'PATCH', 'PUT'].include?(override_method.upcase)
-          http_method = override_method.upcase
-        end
+        http_method = override_method.upcase if override_method && %w[DELETE PATCH PUT].include?(override_method.upcase)
       end
 
       env['REQUEST_METHOD'] = http_method
-      
-      if mapping = routes[path + http_method]
+
+      if (mapping = routes[path + http_method])
         env.merge!(controller_action(mapping))
         app.call(env)
       else
@@ -31,27 +31,27 @@ module Frack
     end
 
     def controller_action(mapping)
-      Hash[%w(controller action).zip(mapping.split('#'))]
+      Hash[%w[controller action].zip(mapping.split('#'))]
     end
 
     def push(route)
-      routes.merge!(route.keys.first + 'PUSH' => route.values.first)
+      routes.merge!("#{route.keys.first}PUSH" => route.values.first)
     end
- 
+
     def delete(route)
-      routes.merge!(route.keys.first + 'DELETE' => route.values.first)
+      routes.merge!("#{route.keys.first}DELETE" => route.values.first)
     end
-    
+
     def patch(route)
-      routes.merge!(route.keys.first + 'PATCH' => route.values.first)
+      routes.merge!("#{route.keys.first}PATCH" => route.values.first)
     end
- 
+
     def post(route)
-      routes.merge!(route.keys.first + 'POST' => route.values.first)
+      routes.merge!("#{route.keys.first}POST" => route.values.first)
     end
- 
+
     def get(route)
-      routes.merge!(route.keys.first + 'GET' => route.values.first)
+      routes.merge!("#{route.keys.first}GET" => route.values.first)
     end
   end
 end
