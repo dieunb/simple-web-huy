e329291a836c4b36ed3d0ea37de241a27ab1e68e
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-10 20:38:26 +0700

    Remove unnecessary autoload

--- lib/frack.rb
+++ lib/frack.rb
@@ -7,10 +7,3 @@ require 'tilt'
 require 'erb'
 require 'active_record'
 require 'pg'
-
-# Frack web framework module
-module Frack
-  autoload :Application,     'frack/application'
-  autoload :BaseController,  'frack/base_controller'
-  autoload :Router,          'frack/router'
-end
