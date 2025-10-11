c2bc67172233b049870d6c1924765e8818471580
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-11 20:25:41 +0700

    Add render method

--- lib/frack/base_controller.rb
+++ lib/frack/base_controller.rb
@@ -6,5 +6,11 @@ module Frack
       @request = Rack::Request.new(env)
       @flash_message = request.session&.delete('flash')
     end
+
+    def render(view)
+      render_template('layouts/application') do
+        render_template(view)
+      end 
+    end
   end
 end
