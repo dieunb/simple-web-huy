251f9d9de6740fbd22704e23c813b527d01e643e
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-12 13:22:21 +0700

    Add Categories table migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_251_012_051_234) do
+ActiveRecord::Schema[8.0].define(version: 20_251_012_060_000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_catalog.plpgsql'
+
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'order_items', force: :cascade do |t|
     t.bigint 'order_id', null: false
