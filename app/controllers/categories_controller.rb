ab733d1beba6a701406a180035c087ac07243c62
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-18 02:59:27 +0700

    Add check sign in for method new in categoriescontroller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,10 @@ class CategoriesController < Frack::BaseController
   end
 
   def new
+    unless current_user
+      request.session['flash'] = 'You must sign in to continue'
+      return [[], 302, { 'location' => '/' }]
+    end
     render 'categories/new'
   end
 
