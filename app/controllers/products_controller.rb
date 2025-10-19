5fbc61f91eb1d7c0fcdddca3a37c6faffa6ec177
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-19 16:31:32 +0700

    Create ProductsController

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# Controller for Product
+class ProductsController < Frack::BaseController
+  def index
+    unless current_user
+      request.session['flash'] = 'You must sign in to continue'
+      return [[], 302, { 'location' => '/' }]
+    end
+
+    @products = Product.all
+    render 'products/index'
+  end
+
+  def new
+    render 'products/new'
+  end
+
+  def create
+    product_params = request.params.slice(
+      'name',
+      'brand',
+      'category_id',
+      'year',
+      'price'
+    )
+
+    new_product = Product.new(product_params)
+
+    if new_product.save
+      request.session['flash'] = 'Product created'
+      [[], 302, { 'location' => '/products' }]
+    else
+      request.session['flash'] = 'Create failed'
+      [[], 302, { 'location' => '/products/new' }]
+    end
+  end
+end
