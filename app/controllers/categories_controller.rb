84336246d36a2304436a41a9aaf946a14f7b0017
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-10-26 22:26:13 +0700

    Add action "show" for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,22 +3,25 @@
 # Controller for Category
 class CategoriesController < Frack::BaseController
   def index
-    unless current_user
-      request.session['flash'] = 'You must sign in to continue'
-      return [[], 302, { 'location' => '/' }]
-    end
+    return require_authentication unless current_user
 
-    page = request.params['page'] || 1
-    @pagy, @categories = pagy(Category.order(:id), page: page)
+    @pagy, @categories = setup_pagination(Category.order(:id))
     render 'categories/index'
   end
 
   def new
-    unless current_user
-      request.session['flash'] = 'You must sign in to continue'
-      return [[], 302, { 'location' => '/' }]
-    end
+    return require_authentication unless current_user
+
     render 'categories/new'
+  end
+
+  def show
+    return require_authentication unless current_user
+
+    return category_not_found unless find_category
+
+    @pagy, @products = setup_pagination(@category.products.order(:id))
+    render 'categories/show'
   end
 
   def create
@@ -30,6 +33,16 @@ class CategoriesController < Frack::BaseController
   end
 
   private
+
+  def find_category
+    category_id = request.params['id']
+    @category = Category.find_by(id: category_id)
+  end
+
+  def category_not_found
+    request.session['flash'] = 'Category not found'
+    [[], 302, { 'location' => '/categories' }]
+  end
 
   def creation_outcome(name)
     if name.to_s.strip.empty?
