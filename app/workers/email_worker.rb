2686e7ad21597fe41d6e009ffa178de28765cc77
xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-01-19 20:28:57 +0700

    Implement EmailWorker to send email

---
+++ app/workers/email_worker.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+
+# Background worker for sending emails asynchronously
+class EmailWorker
+  include Sidekiq::Worker
+
+  def perform(user_id)
+    user = User.find_by(id: user_id)
+    return unless user
+
+    EmailService.send_welcome_email(user)
+  end
+end
