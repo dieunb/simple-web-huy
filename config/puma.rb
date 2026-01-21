a9be68b200f5224b4eddb49078e160b00e7b3273
xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-01-21 17:34:17 +0700

    Fix error by rubocop

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'logger'
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch('WEB_CONCURRENCY', 2)
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch('PORT', 3000)
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch('RAILS_ENV', 'development')
 
 log_requests true
-custom_logger Logger.new(STDOUT)
+custom_logger Logger.new($stdout)
 
 log_formatter do |str|
   "[#{Process.pid}] [#{Socket.gethostname}] #{Time.now}: #{str}"
@@ -19,6 +21,6 @@ on_worker_boot do
   # Configuration for frameworks like Rails might be placed here if running in cluster mode
   if defined?(ActiveRecord)
     ActiveRecord::Base.establish_connection
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    ActiveRecord::Base.logger = Logger.new($stdout)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,60 +12,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_12_060000) do
+ActiveRecord::Schema[8.0].define(version: 20_251_012_060_000) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension 'pg_catalog.plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.bigint "order_id", null: false
-    t.bigint "product_id", null: false
-    t.integer "quantity", default: 1, null: false
-    t.decimal "list_price", precision: 10, scale: 2, null: false
-    t.decimal "discount", precision: 5, scale: 2, default: "0.0"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
+  create_table 'order_items', force: :cascade do |t|
+    t.bigint 'order_id', null: false
+    t.bigint 'product_id', null: false
+    t.integer 'quantity', default: 1, null: false
+    t.decimal 'list_price', precision: 10, scale: 2, null: false
+    t.decimal 'discount', precision: 5, scale: 2, default: '0.0'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['order_id'], name: 'index_order_items_on_order_id'
+    t.index ['product_id'], name: 'index_order_items_on_product_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "status", null: false
-    t.date "order_date"
-    t.date "required_date"
-    t.date "shipped_date"
-    t.integer "store_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_date"], name: "index_orders_on_order_date"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'status', null: false
+    t.date 'order_date'
+    t.date 'required_date'
+    t.date 'shipped_date'
+    t.integer 'store_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['order_date'], name: 'index_orders_on_order_date'
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "brand"
-    t.integer "category_id"
-    t.integer "year"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["brand"], name: "index_products_on_brand"
+  create_table 'products', force: :cascade do |t|
+    t.string 'name'
+    t.string 'brand'
+    t.integer 'category_id'
+    t.integer 'year'
+    t.decimal 'price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['brand'], name: 'index_products_on_brand'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", limit: 255, null: false
-    t.string "password_digest", limit: 255, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', limit: 255, null: false
+    t.string 'password_digest', limit: 255, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "products"
+  add_foreign_key 'order_items', 'orders'
+  add_foreign_key 'order_items', 'products'
 end
