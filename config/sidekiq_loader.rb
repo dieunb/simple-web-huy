35901c657c5726b0a2f3043891d475bd7dc267f2
xuanhuy218972 <xuanhuy21082023@gmail.com>
2026-01-26 20:59:27 +0700

    Add Sidekiq loader

---
+++ config/sidekiq_loader.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
+
+$LOAD_PATH.unshift << '.'
+
+require 'lib/frack'
+require 'config/initializers/sidekiq'
+
+# Load models
+require 'app/models/user'
+require 'app/models/category'
+require 'app/models/product'
+
+# Load services
+require 'app/services/email_service'
+
+# Load workers
+require 'app/workers/email_worker'
